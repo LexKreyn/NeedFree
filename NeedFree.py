45b184f190464b5c1ea7996bfe564574651704fe
LexKreyn <48969431+LexKreyn@users.noreply.github.com>
2025-07-02 18:19:51 +0400

    Update NeedFree.py

--- NeedFree.py
+++ NeedFree.py
@@ -10,7 +10,7 @@ import bs4
 import re
 
 
-API_URL_TEMPLATE = "https://store.steampowered.com/search/results/?query&start={pos}&count=100&specials=1&infinite=1"
+API_URL_TEMPLATE = "https://store.steampowered.com/search/results/?query&start={pos}&count=100&infinite=1"
 THREAD_CNT = 8
 
 free_list = queue.Queue()
@@ -52,6 +52,7 @@ def get_free_goods(start, append_list = False):
             full_discounts_div = page_parser.find_all(name = "div", attrs = {"class":"search_discount_block"})
             sub_free_list = [
                 [
+                    None,
                     div.parent.parent.parent.parent.find(name="div", attrs={"class": "search_discount_block"}).get("data-discount"),  # discount
                     div.parent.parent.parent.parent.find(name="div", attrs={"class": "discount_original_price"}),  # full price
                     div.parent.parent.parent.parent.find(name="div", attrs={"class": "discount_final_price"}).get_text(),  # price with discount
@@ -60,6 +61,7 @@ def get_free_goods(start, append_list = False):
                     div.parent.parent.parent.parent.find_all("div")[0].find("img").get("src"),  # image
                     div.parent.parent.parent.parent.get("data-ds-tagids"),  # tags
                     div.parent.parent.parent.parent.get("data-ds-bundleid"),  # is_bundle
+                    div.parent.parent.parent.parent.get("data-ds-bundle-data"),  # bundle_data
                     div.parent.parent.parent.parent.find(name="span", attrs={"class": "music"}),  # is_soundtrack
                     div.parent.parent.parent.parent.find(name="span", attrs={"class": "win"}),  # for win
                     div.parent.parent.parent.parent.find(name="span", attrs={"class": "mac"}),  # for mac
@@ -73,19 +75,22 @@ def get_free_goods(start, append_list = False):
             counter = 0
             if append_list:
                 for sub_free in sub_free_list:
-                    if sub_free[0] and sub_free[0] != '0':
-                        if sub_free[1]:
-                            sub_free[1] = sub_free[1].get_text()
-                        if sub_free[13]:
-                            sub_free[13] = sub_free[13].get_text()
+                    if sub_free[1] and sub_free[1] != '0':
+                        if sub_free[2]:
+                            sub_free[2] = sub_free[2].get_text()
+
+                        if sub_free[15]:
+                            sub_free[15] = sub_free[15].get_text()
                         else:
-                            sub_free[13] = ''
-                        if sub_free[14]:
-                            sub_free[14] = sub_free[14].get("data-tooltip-html")
+                            sub_free[15] = ''
+
+                        if sub_free[16]:
+                            sub_free[16] = sub_free[16].get("data-tooltip-html")
                         else:
-                            sub_free[14] = r'None<br>0% of the 0'
+                            sub_free[16] = r'None<br>0% of the 0'
+
                         counter += 1
-                        sub_free[0] = int(sub_free[0])
+                        sub_free[1] = int(sub_free[1])
                         free_list.put(sub_free)
 
             return goods_count
@@ -114,7 +119,7 @@ free_ids = set()
 while not free_list.empty():
     free_item = free_list.get()
 
-    game_link = free_item[4]
+    game_link = free_item[5]
 
     game_id = re.search(r'.com\/[a-z]+\/(\d+)\/', game_link)
     if game_id is None:
@@ -124,31 +129,36 @@ while not free_list.empty():
     if game_id in free_ids:
         continue
 
-    game_tags = free_item[6]
-    free_item[6] = json.loads(game_tags or '[]')
+    free_item[0] = game_id
 
-    is_bundle = free_item[7]
-    free_item[7] = bool(is_bundle)
+    game_tags = free_item[7]
+    free_item[7] = json.loads(game_tags or '[]')
 
-    is_soundtrack = free_item[8]
-    free_item[8] = bool(is_soundtrack)
+    is_bundle = free_item[8]
+    free_item[8] = bool(is_bundle)
 
-    for_win = free_item[9]
-    free_item[9] = bool(for_win)
+    bundle_data = free_item[9]
+    free_item[9] = json.loads(bundle_data or r'{}')
 
-    for_mac = free_item[10]
-    free_item[10] = bool(for_mac)
+    is_soundtrack = free_item[10]
+    free_item[10] = bool(is_soundtrack)
 
-    for_linux = free_item[11]
-    free_item[11] = bool(for_linux)
+    for_win = free_item[11]
+    free_item[11] = bool(for_win)
 
-    for_vr = free_item[12]
-    free_item[12] = bool(for_vr)
+    for_mac = free_item[12]
+    free_item[12] = bool(for_mac)
 
-    release = free_item[13]
-    free_item[13] = release.strip() or ''
+    for_linux = free_item[13]
+    free_item[13] = bool(for_linux)
 
-    reviews = free_item[14]
+    for_vr = free_item[14]
+    free_item[14] = bool(for_vr)
+
+    release = free_item[15]
+    free_item[15] = release.strip() or ''
+
+    reviews = free_item[16]
     if reviews is None:
         reviews = r'None<br>0% of the 0'
     reviews = re.search(r'^([a-zA-Z ]+)\<br\>(\d{1,3})% of the ([\d,]+)', reviews)
@@ -156,12 +166,12 @@ while not free_list.empty():
         review, percent, users = 'None', '0', '0'
     else:
         review, percent, users = reviews.groups()
-    free_item[14] = [review, int(percent), int(users.replace(',' ,''))]
+    free_item[16] = [review, int(percent), int(users.replace(',' ,''))]
 
     free_ids.add(game_id)
     final_free_list.append(free_item)
 
-final_free_list = sorted_data = sorted(final_free_list, key=lambda x: (-x[0], x[3]))
+final_free_list = sorted_data = sorted(final_free_list, key=lambda x: (-x[1], x[4]))
 
 with open("free_goods_detail.json", "w", encoding="utf-8") as fp:
     json.dump({
